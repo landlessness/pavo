dcdf9512d9657dd1005e6dd9b34ca89ce7515ec4
landlessness <brian@landlessness.net>
2012-02-25 14:33:25 -0500

    rails app shell

---
+++ rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prails::Application.config.secret_token = '0ad7d6e29bb5f740590951501c723af82a974676c58380fa5682cbd1740da4cc58b2a0c82379da80e160858216fad6444a96b863a22e22d1138767b4b9481f05'
